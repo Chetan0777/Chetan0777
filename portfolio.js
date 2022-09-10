dfbf257daee2cd149cc6fc8b0177a45c67add056
Chetan0777 <coderchetan1@gmail.com>
2022-09-10 14:03:06 +0530

    done

--- portfolio.js
+++ portfolio.js
@@ -2,11 +2,11 @@ let tablinks = document.getElementsByClassName("tab-links");
 let tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
+    for(let i of tablinks){
+        i.classList.remove("active-link");
     }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
+    for(let i of tabcontents){
+        i.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
