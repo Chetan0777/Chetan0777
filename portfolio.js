b2844914eeb2126c0f5d36c4a7231a2a5811c43e
Chetan0777 <coderchetan1@gmail.com>
2022-09-11 15:50:51 +0530

    done

--- portfolio.js
+++ portfolio.js
@@ -11,3 +11,19 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+
+let side = document.getElementById("sidemenu");
+
+
+function openmenu(){
+
+    side.style.right = "0";
+
+}
+
+function closemenu(){
+    
+    side.style.right = "-200px";
+
+}
